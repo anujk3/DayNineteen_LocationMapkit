bd526c072a8b759c6885afb9b6540c0f90bcc9c7
Anuj Katiyal <anujk3@gmail.com>
2014-10-02 15:30:55 +0530

    Initial Commit

---
+++ DayNineteen_LocationMapkit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DayNineteen_LocationMapkit
+//
+//  Created by Anuj Katiyal on 02/10/14.
+//  Copyright (c) 2014 Katiyals. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DayNineteen_LocationMapkit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DayNineteen_LocationMapkit
+//
+//  Created by Anuj Katiyal on 02/10/14.
+//  Copyright (c) 2014 Katiyals. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
